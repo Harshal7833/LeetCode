30134a34c8f50c837dc5884d7a28fad3021ea087
Harshal7833 <vanjariharshal5@gmail.com>
2026-07-06 11:17:44 +0530

    Time: 2157 ms (5.38%), Space: 42.2 MB (45.36%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int i = 1;
+        int sum = 0;
+        int temp = num;
+        while (i != num){
+            if ( num % i == 0){
+                sum = sum + i;
+            }
+            i++;
+        }
+        if (temp == sum) return true;
+        return false;
+    }
+}
